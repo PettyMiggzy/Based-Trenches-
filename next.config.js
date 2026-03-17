7b091efd59251dd5d9e6cdb509b91386d76f803b
PettyMiggzy <bahmed3170@gmail.com>
2026-03-17 18:06:15 -0400

    fix merge conflict in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
->>>>>>> b36b55d6bbfb41193aa17013b544f6ad108a8934
   images: {
     domains: [],
   },
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> b36b55d6bbfb41193aa17013b544f6ad108a8934
 module.exports = nextConfig
